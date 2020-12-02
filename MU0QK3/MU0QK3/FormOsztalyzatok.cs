b2ab084eba82eb0d43abea59297a390d360a13c3
Marci Mihajlovits <mihajlovitsmarci@freemail.hu>
2020-12-02 17:47:26 +0100

    README

--- MU0QK3/MU0QK3/FormOsztalyzatok.cs
+++ MU0QK3/MU0QK3/FormOsztalyzatok.cs
@@ -14,14 +14,21 @@ namespace MU0QK3
     {
         Database1Entities context = new Database1Entities();
         List<Jegyek> jegyek = new List<Jegyek>();
+        public Jegyek aktjegy = new Jegyek();
         public FormOsztalyzatok()
         {
             InitializeComponent();
             feltolt();
             kiir();
-            
+            listBoxJegyek.SelectedIndexChanged += ListBoxJegyek_SelectedIndexChanged;
 
         }
+
+        private void ListBoxJegyek_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            aktjegy = (Jegyek)listBoxJegyek.SelectedItem;
+        }
+
         public void feltolt()
         {
             foreach (var item in context.Jegyeks)
@@ -35,17 +42,12 @@ namespace MU0QK3
             listBoxJegyek.DisplayMember = "Jegy";
             listBoxJegyek.ValueMember = "Id";
         }
+        
 
         public void kiir()
         {
             labelNev.Text = FormUjJegy.akttan.Név + " osztályzatai";
-            foreach (var item in jegyek)
-            {
-                
-                {
-
-                }
-            }
+            labelDatum.Text = aktjegy.Dátum.ToString();
         }
     }
 }
