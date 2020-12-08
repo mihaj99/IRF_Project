2027d2b9ecf4a6fbe0e740bc90e1bd6b78c279da
Marci Mihajlovits <mihajlovitsmarci@freemail.hu>
2020-12-08 17:37:38 +0100

    Atlag_kiir

--- MU0QK3/MU0QK3/FormAtlagok.cs
+++ MU0QK3/MU0QK3/FormAtlagok.cs
@@ -15,7 +15,8 @@ namespace MU0QK3
         List<atlagok> _atlagok = new List<atlagok>();
         List<Tanulok> _tanulok = new List<Tanulok>();
         List<Jegyek> _jegyek = new List<Jegyek>();
-        int szamlalo = 0;
+        int szamlalo;
+        int maxhossz = 0;
 
         Database1Entities context = new Database1Entities();
         double osszeg = 0;
@@ -27,9 +28,74 @@ namespace MU0QK3
         public FormAtlagok()
         {
             InitializeComponent();
+            Atlagszamitas();
+            Diagram();
+            Nevek();
+            MaximumHossz();
+            AtlagKiir();
+        }
 
+        private void AtlagKiir()
+        {
+            szamlalo = 0;
+            foreach (var item in _atlagok)
+            {
+                Label lbl = new Label();
+                lbl.Text = item.atlag.ToString();
+                lbl.Left = 1 + maxhossz;
+                lbl.Top = 1 + szamlalo * lbl.Height;
+                panel1.Controls.Add(lbl);
+                szamlalo++;
 
+            }
+        }
 
+        private void MaximumHossz()
+        {
+            foreach (Label item in panel1.Controls)
+            {
+                if (item.Width > maxhossz)
+                {
+                    maxhossz = item.Width;
+                }
+            }
+        }
+
+        private void Nevek()
+        {
+            szamlalo = 0;
+            foreach (var item in _atlagok)
+            {
+                Label lbl = new Label();
+                lbl.Text = item.nev;
+                lbl.Left = 1;
+                lbl.Top = 1 + szamlalo * lbl.Height;
+                panel1.Controls.Add(lbl);
+                szamlalo++;
+
+            }
+        }
+
+        private void Diagram()
+        {
+            chartAtlagok.DataSource = _atlagok;
+            var series = chartAtlagok.Series[0];
+            series.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Column;
+            series.XValueMember = "nev";
+            series.YValueMembers = "atlag";
+            series.BorderWidth = 2;
+
+            var legend = chartAtlagok.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartAtlagok.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
+        }
+
+        private void Atlagszamitas()
+        {
             foreach (var tanulo in context.Tanuloks)
             {
                 osszeg = 0;
@@ -50,34 +116,6 @@ namespace MU0QK3
                 ujatlag.nev = tanulo.Név;
                 ujatlag.atlag = osszeg / darabszam;
                 _atlagok.Add(ujatlag);
-
-            }
-            chartAtlagok.DataSource = _atlagok;
-            var series = chartAtlagok.Series[0];
-            series.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Column;
-            series.XValueMember = "nev";
-            series.YValueMembers = "atlag";
-            series.BorderWidth = 2;
-
-            var legend = chartAtlagok.Legends[0];
-            legend.Enabled = false;
-
-            var chartArea = chartAtlagok.ChartAreas[0];
-            chartArea.AxisX.MajorGrid.Enabled = false;
-            chartArea.AxisY.MajorGrid.Enabled = false;
-            chartArea.AxisY.IsStartedFromZero = false;
-
-            this.Controls.Add(chartAtlagok);
-
-
-            foreach (var item in _atlagok)
-            {
-                Label lbl = new Label();
-                lbl.Text = item.nev;
-                lbl.Left = 1;
-                lbl.Top = 1 + szamlalo * lbl.Height;
-                panel1.Controls.Add(lbl);
-                szamlalo++;
 
             }
         }
